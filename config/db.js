505696fecb881b21d3d9c61bf7ff32b37504fb4b
kevin.romero@telus.com <kevin.romero@telusinternational.com>
2021-04-17 17:31:18 -0600

    Setup a MongoDB cluster online. Installed mongoose and created the db connection module.

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require ('mongoose')
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect('mongodb+srv://buglogger:buglogger2021@cluster0.ztrrh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+        {
+         useNewUrlParser: true,
+         useCreateIndex: true,
+         useUnifiedTopology: true,   
+        })
+        console.log('MongoDB is now connected')
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
